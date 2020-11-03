2768511b0b1c3fb877212cbb66512f48342de9ca
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-04 09:40:03 +1100

    Added a basic hook example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld'
+import CounterExample from './components/CounterExample'
 
 
 function App() {
   return (
    <div>
-     <HelloWorld name="Callum"/>
+     {/* <HelloWorld name="Callum"/> */}
+     <CounterExample />
    </div>
   );
 }
